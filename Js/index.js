09133e10f0914e0d22fdf7df63be2bcacf4e343f
KevHLJob <KevHLJob@github.com>
2022-04-23 12:52:16 -0600

    function encriptar

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,21 @@
 
 
-function Encriptar(){
+function encriptar (){
+    //create a var save value of txt word.
+var frase = document.getElementById("word").value;
+const mapWord = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat"
+};
+frase = frase.replace(/(?:a|e|i|o|u)/g, matched => mapWord[matched].toLowerCase() );
 
-    
+var lbnew = document.getElementById('text-encript');
+lbnew.innerHTML=`${frase}`;
+console.log(frase);
+
 }
+
+
